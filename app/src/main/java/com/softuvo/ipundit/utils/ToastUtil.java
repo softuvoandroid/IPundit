42fc8a4b9c4ad67a55bace4a47f7078af68eb3d5
Nikhil Guleria <Nikhil Guleria>
2017-06-13 11:13:58 +0530

    Implemented Toast methods

--- app/src/main/java/com/softuvo/ipundit/utils/ToastUtil.java
+++ app/src/main/java/com/softuvo/ipundit/utils/ToastUtil.java
@@ -1,9 +1,20 @@
 package com.softuvo.ipundit.utils;
 
+import android.widget.Toast;
+
+import com.softuvo.ipundit.config.App;
+
 /**
  * Created by softuvo on 6/13/2017.
  */
 
 public class ToastUtil {
-    
+    public static void showSmallToast(String message){
+        Toast.makeText(App.getAppContext(), message, Toast.LENGTH_SHORT).show();
+    }
+
+    public static void showLongToast(String message){
+        Toast.makeText(App.getAppContext(), message, Toast.LENGTH_LONG).show();
+    }
+
 }
